09143ccc16dd5f9f0cc88f05bc5a75d1cbf5be67
ihsanye <ihsany98@gmail.com>
2024-09-12 18:56:20 +0300

    request header parser microservice 2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/api/whoami', function (req, res) {
   res.json({
     ipaddress: req.socket.remoteAddress,
     language: req.headers['accept-language'],
-    software: req.headers.['user-agent']
+    software: req.headers['user-agent']
   });
 });
 
